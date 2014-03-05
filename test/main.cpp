9ac9836c77cb7ea6a49ecdb099c98b476a9b531c
yolandamartre <yolandamartre@hotmail.com>
2014-03-05 14:25:38 -0600

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Yolanda Martínez on 3/5/14.
+//  Copyright (c) 2014 Yolanda Martínez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
