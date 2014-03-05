08f44e70f30a635a46f5828c6417c61b4c43317c
yolandamartre <yolandamartre@hotmail.com>
2014-03-05 15:36:47 -0600

    agrego la clase

---
+++ test/Clase.h
@@ -0,0 +1,21 @@
+//
+//  Clase.h
+//  test
+//
+//  Created by Yolanda Martínez on 3/5/14.
+//  Copyright (c) 2014 Yolanda Martínez. All rights reserved.
+//
+
+#ifndef test_Clase_h
+#define test_Clase_h
+
+class Clase
+{
+public:
+    Clase() { x = 20; }
+    void muestra() { std::cout << x << std::endl; }
+private:
+    int x;
+};
+
+#endif
